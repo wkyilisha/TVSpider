5a3b8d984b5a411c2cb41c6d57e5d7756656896f
wkyilisha <106513551+wkyilisha@users.noreply.github.com>
2026-05-17 16:13:13 +0800

    Update DanmuHelper.java

--- app/src/main/java/com/github/catvod/spider/DanmuHelper.java
+++ app/src/main/java/com/github/catvod/spider/DanmuHelper.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import java.util.Random;
 
 /**
- * DanmuHelper - 针对 TVSpider 项目优化的弹幕助手
+ * DanmuHelper - 针对TVSpider 项目优化的弹幕助手
  * 功能：多源搜索弹幕、JSON转XML、广告过滤、本地代理响应
  */
 public class DanmuHelper {
