d0495e8a8b94227f9ccd4c9ad467d248df2737b4
wkyilisha <106513551+wkyilisha@users.noreply.github.com>
2026-05-28 11:57:38 +0800

    Update TvDy.java

--- app/src/main/java/com/github/catvod/spider/TvDy.java
+++ app/src/main/java/com/github/catvod/spider/TvDy.java
@@ -23,6 +23,7 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
 
 public class TvDy extends Spider {
