639ecd60b13019cf4928b435f897f6e5ff99d384
wkyilisha <106513551+wkyilisha@users.noreply.github.com>
2026-06-01 14:46:58 +0800

    Update OkResult.java

--- app/src/main/java/com/github/catvod/net/OkResult.java
+++ app/src/main/java/com/github/catvod/net/OkResult.java
@@ -10,17 +10,27 @@ public class OkResult {
 
     private final int code;
     private final String body;
+    private final byte[] bodyBytes;
     private final Map<String, List<String>> resp;
 
     public OkResult() {
         this.code = 500;
         this.body = "";
+        this.bodyBytes = null;
         this.resp = new HashMap<>();
     }
 
     public OkResult(int code, String body, Map<String, List<String>> resp) {
         this.code = code;
         this.body = body;
+        this.bodyBytes = null;
+        this.resp = resp;
+    }
+
+    public OkResult(int code, byte[] bodyBytes, Map<String, List<String>> resp) {
+        this.code = code;
+        this.body = null;
+        this.bodyBytes = bodyBytes;
         this.resp = resp;
     }
 
@@ -30,6 +40,10 @@ public class OkResult {
 
     public String getBody() {
         return TextUtils.isEmpty(body) ? "" : body;
+    }
+
+    public byte[] getBodyBytes() {
+        return bodyBytes;
     }
 
     public Map<String, List<String>> getResp() {
